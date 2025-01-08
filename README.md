2647e6fe9e49b24bf9fdf1846aa1cd0bb04f55e9
陈志峰 <chenzhifeng@yupaowang.com>
2025-01-08 11:56:56 +0800

    feat: yp-link项目开发

--- README.md
+++ README.md
@@ -1,31 +1,16 @@
 # yp-link README
 
-This is the README for your extension "yp-link". After writing up a brief description, we recommend including the following sections.
+因为yp项目组规范要求。分支名必须是 特征/飞书id-name且commit的时候也必须带上飞书项目id，所以为了
+1. 方便开发commit，便捷复制飞书id；
+2. 方便打开项目文档。不需要特意去找对应的飞书项目，直接从编辑器打开文档
 
 ## Features
-
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
-
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+1. 复制分支名称
+2. 复制飞书ID
+3. 打开飞书项目文档
 
 ## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
+run `YP-LINK: Show Branch And FeishuId` command
 
 ## Known Issues
 
@@ -35,37 +20,8 @@ Calling out known issues can help limit users opening duplicate issues against y
 
 Users appreciate release notes as you update your extension.
 
-### 1.0.0
+### 0.0.1
 
 Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,158 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+interface IMessage {
+  command: "showMessage" | "showErrorMessage" | "openBrowser";
+  url?: string;
+  text?: string;
+}
+const extractFeishuId = (branchName: string) => {
+  const reg = /.*[-|\/](?<id>\d+)/gm;
+  let match = reg.exec(branchName);
+  const id = match?.groups?.id ?? "";
+  //   while ((match = reg.exec(branchName))) {
+  //     const id = match.groups?.id;
+  //     id !== undefined && ids.push(id);
+  //   }
+  return id;
+};
+
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand("extention.ypLink", () => {
+    function getBranchName() {
+      const gitExtension = vscode.extensions.getExtension("vscode.git");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "yp-link" is now active!');
+      if (!gitExtension) {
+        vscode.window.showErrorMessage("Git extention not found");
+        return;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('yp-link.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from yp-link!');
-	});
+      const gitApi = gitExtension.exports.getAPI(1);
+      const repo = gitApi.repositories[0];
 
-	context.subscriptions.push(disposable);
+      const branchName = repo.state.HEAD?.name ?? "";
+
+      return branchName;
+    }
+
+    const branchName = getBranchName();
+    if (!branchName) {
+      return;
+    }
+
+    const feishuId = extractFeishuId(branchName);
+
+    const webviewPannel = vscode.window.createWebviewPanel(
+      "branchAndFeishuidView",
+      "Show Branch And FeishuId",
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true,
+      }
+    );
+
+    webviewPannel.webview.html = getWebviewContent(branchName, feishuId);
+
+    webviewPannel.webview.onDidReceiveMessage((message: IMessage) => {
+      switch (message.command) {
+        case "showMessage":
+          vscode.window.showInformationMessage(message.text ?? "");
+          break;
+        case "showErrorMessage":
+          vscode.window.showErrorMessage(message.text ?? "");
+          break;
+        case "openBrowser":
+          if (!message.url) return;
+          const url = vscode.Uri.parse(message.url);
+          vscode.env.openExternal(url);
+          break;
+        default:
+          break;
+      }
+    });
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function getWebviewContent(branchName: string, feishuId: string) {
+  return `
+	<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Branch Info And FeishuId</title>
+  </head>
+  <body>
+    <p>
+      <span style="padding-right: 20px">
+        <strong>Branch Name:</strong> ${branchName}</span
+      >
+      <button id="copyBranchName">Copy Branch Name</button>
+    </p>
+
+    <p>
+      <span style="padding-right: 20px">
+        <strong>FeishuId:</strong>
+        <span> ${feishuId}</span>
+      </span>
+      <button id="copyFeishuId">Copy 飞书ID</button>
+    </p>
+
+    <p>
+      <button id="openFeishuProject">打开飞书项目</button>
+    </p>
+
+    <script>
+      const vscode = acquireVsCodeApi();
+
+      const branchName = "${branchName}";
+      const feishuId = "${feishuId}";
+
+      const copyBranchNamebtn = document.querySelector("#copyBranchName");
+      copyBranchNamebtn.addEventListener("click", () => {
+        copyText(branchName);
+      });
+
+      const copyFeishuIdSpan = document.querySelector("#copyFeishuId");
+      copyFeishuIdSpan.addEventListener("click", () => {
+        if (!feishuId) {
+          return;
+        }
+        copyText(feishuId);
+      });
+
+      function copyText(text) {
+        navigator.clipboard.writeText(text).then(() => {
+          vscode.postMessage({
+            command: "showMessage",
+            text: text + "copyied to cliboard",
+          });
+        });
+      }
+
+      const openFeishuProjectBtn = document.querySelector("#openFeishuProject");
+      openFeishuProjectBtn.addEventListener("click", () => {
+        try {
+          if (!feishuId) {
+            return;
+          }
+          vscode.postMessage({
+            command: "openBrowser",
+            url: "https://project.feishu.cn/yupao/story/detail/" + feishuId,
+          });
+        } catch (error) {
+          vscode.postMessage({
+            command: "showErrorMessage",
+            text: error?.message || "打开浏览器错误",
+          });
+        }
+      });
+    </script>
+  </body>
+</html>
+	`;
+}
